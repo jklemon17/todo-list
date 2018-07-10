89d3ca67dda29515cb4ce407b7191519dbb089d5
Kyle Lemon <james.kyle.lemon@gmail.com>
2018-07-10 16:00:05 -0400

    add and remove from list object
    Co-authored-by: Paul Rail <paul.a.rail@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ const form = document.getElementById("list");
 const title = document.getElementById("title");
 const newItem = document.getElementById("new-item");
 
-newItem.addEventListener('keypress', saveItem);
-
 function saveItem(e) {
   if (e.keyCode == 13) {
       e.preventDefault();
-      showNewItem(this.value);
+      list.addItem(this.value);
+      displayList(list);
       this.value = "";
       this.placeholder = "Add another item";
   }
@@ -25,8 +24,12 @@ function showNewItem(content, dueDate, priority) {
   dateInput.value = dueDate;
 
   let prioritySelect = document.createElement('select');
-  prioritySelect.type = "low, medium, urgent";
-  prioritySelect.option = priority;
+  let priorities = ['low', 'medium', 'high'];
+  for (i = 0; i < priorities.length; i++) {
+     let p = new Option(priorities[i]);
+     prioritySelect.options.add(p);
+  }
+  prioritySelect.selectedIndex = priorities.indexOf(priority);
 
   let deleteButton = document.createElement('div');
   deleteButton.innerHTML = "X";
@@ -41,21 +44,47 @@ function showNewItem(content, dueDate, priority) {
 
 // remove and item:
 function deleteItem() {
-  this.parentNode.parentNode.removeChild(this.parentNode);
+  list.removeItem(this.parentNode.firstChild.value);
+  console.log(list);
+  displayList(list);
 }
 
+function displayList(list) {
+  while (form.firstChild) {
+    form.removeChild(form.firstChild);
+  }
+  //should be separate displayTitle function
+  let title = document.createElement('input');
+  title.value = list.title;
+  form.appendChild(title);
+
+  form.appendChild(document.createElement('br'));
+
+  //should be separate displayInput function
+  let input = document.createElement('input');
+  input.placeholder = "Add an item";
+  input.addEventListener('keypress', saveItem);
+  form.appendChild(input);
+
+  list.items.forEach(item => showNewItem(item.content, item.dueDate, item.priority));
+}
 
 const listFactory = (title="Untitled List", description="", items=[]) => {
-  const addItem = (content,dueDate,priority) => {
-     items.push(itemFactory(content,dueDate,priority));
+  const addItem = (content, dueDate, priority) => {
+     items.push(itemFactory(content, dueDate, priority));
   }
-  const removeItem = () => "funtion to remove item";
+  const removeItem = (content) => {
+    index = items.indexOf(items.find(x => x.content == content))
+    items.splice(index, 1)
+    // for (i in items) {
+    //   if (content == items[i].content) {
+    //     items.splice(i, 1);
+    //     break;
+    //   }
+    // }
+  }
   return { title, description, items, addItem, removeItem }
 };
-
-
-const firstList = listFactory("Groceries", "For the party!");
-
 
 const itemFactory = (content="New to-do item", dueDate=new Date()+1, priority="low" ) => {
   const setContent = (content) => "function to edit the content";
@@ -64,22 +93,12 @@ const itemFactory = (content="New to-do item", dueDate=new Date()+1, priority="l
   return { content, dueDate, priority, setContent, setDate, setPriority }
 };
 
+const list = listFactory("Groceries", "For the party!");
 
+list.addItem("Grab bananas", new Date("08-10-2018"), "medium");
+list.addItem("Grab napkins",new Date("08-10-2018"),"low");
+list.addItem("Grab cake",new Date("08-10-2018"),"high");
 
-firstList.addItem("Grab bananas", new Date("08-10-2018"), "medium");
-firstList.addItem("Grab napkins",new Date("08-10-2018"),"low");
-firstList.addItem("Grab cake",new Date("08-10-2018"),"high");
+console.log(list);
 
-console.log(firstList);
-
-// function displayLists(list) {
-//
-//   // create the first two inputs?
-//   <input id="title" type="text" placeholder="Untitled"></br>
-//   <input id="new-item" type="text" name="new-item" value="" placeholder="Type your first to-do item">
-//
-//   // add each item:
-//   list.items.forEach(createNewItem(text));
-// }
-//
-// displayLists(firstList);
+displayList(list);
